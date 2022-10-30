4cfa463798fd432e6f9344725ec24571abbaee1d
Ali Hassaine <ali.hs@outlook.fr>
2022-10-30 15:36:45 +0000

    Add order array update on model creation

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,4 +1,14 @@
 class Column < ApplicationRecord
   has_many :tasks
   belongs_to :board
+  after_create :update_board_col_order
+
+
+
+  private
+
+  def update_board_col_order
+    board.col_order_ids << id
+    board.save
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :column
+
+  after_create :update_col_task_order
+
+
+  def update_col_task_order
+    old_tasks = column.task_orders
+    column.update_attribute(:task_orders, old_tasks << id)
+  end
 end
