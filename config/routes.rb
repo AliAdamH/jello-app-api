69ed3bcd1704533aee1ad665f4148366f19f0c83
Ali Hassaine <ali.hs@outlook.fr>
2023-04-03 19:07:47 +0000

    Remove useless named route param

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
       get '/tasks/:id', to: 'tasks#show'
       put '/order_tasks/:id', to: 'columns#update_task_orders'
       put '/columns', to: 'columns#update'
-      put '/order_columns/:id', to: 'boards#update_column_order'
+      put '/order_columns', to: 'boards#update_column_order'
       put '/labels', to: 'labels#update'
       post '/labels', to: 'labels#create'
       post '/task_labels', to: 'task_labels#create'
